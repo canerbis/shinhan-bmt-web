c4074394fb9b241ba751cb7ab7c2abb40279e48a
canerbis <38413274+canerbis@users.noreply.github.com>
2019-07-01 20:26:34 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,15 @@
 # turn on bash's job control
 set -m
 
-# Start the primary process and put it in the background
-# nginx &
-nginx -g "daemon off" &
 
 # Start the helper process
 /app/main &
+
+# Start the primary process and put it in the background
+ nginx 
+# nginx -g "daemon off" &
+
+
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
