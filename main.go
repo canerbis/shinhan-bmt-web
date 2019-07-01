fe48fdc5139551e3e664775f306b31ecc8a4308a
canerbis <38413274+canerbis@users.noreply.github.com>
2019-07-01 19:08:22 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const htmlIndex = `
+<html><body>
+<h1> Hello world </h1>
+</body><html>
+`
+
+func main() {
+
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/time", handleTime)
+	http.HandleFunc("/version", handleVersion)
+
+	log.Println("Hello Cocktail Server start")
+	fmt.Println(http.ListenAndServe(":3000", nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, htmlIndex)
+	log.Println("Index page was called")
+}
+
+func handleTime(w http.ResponseWriter, r *http.Request) {
+
+	timeStr := "<h1> " + time.Now().String() + " </h1>"
+
+	fmt.Fprintf(w, timeStr)
+	log.Println("Time API was called")
+}
+
+func handleVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> Version 1.9 </h1>")
+	log.Println("Version API was called")
+}
